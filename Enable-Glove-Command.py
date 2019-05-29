a9f431549ff4402fff061cb57ed21fac13422b59
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-05-29 08:57:43 -0500

    BT Scan

--- Enable-Glove-Command.py
+++ Enable-Glove-Command.py
@@ -4,7 +4,8 @@
 # James Andrews <jandrews7348@floridapoly.edu>
 import time
 import sys
-from bluetooth import *
+#from bluetooth import *
+import bluetooth
 from uuid import uuid4
 from bluetooth.ble import DiscoveryService
 
@@ -23,35 +24,17 @@ def working():
                 print(">> System interrupt")
                 exit()
 
-server_sock = BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))#Not secure, but quick
-server_sock.listen(1)
-port = server_sock.getsockname()[1]
+def SearchForDev():
+        for addr, name in nearby_devices:
+                print("  %s - %s" % (addr, name))
+        print("found %d devices" % len(nearby_devices))
 
-advertise_service( server_sock, "SkydioHub",
-                   service_id = UUID,
-                   service_classes = [ UUID, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
-print("Waiting for connection on port %d" % port)
+        # bluetooth low energy scan
+        service = DiscoveryService()
+        devices = service.discover(2)
 
-client_sock, client_info = server_sock.accept()
-print("Connected to: ", client_info)
+        for address, name in devices.items():
+                print("name: {}, address: {}".format(name, address))
 
-def reciever():
-        try:
-                while True:
-                        data = client_sock.recv(1024)
-                        if len(data) == 0: break
-                        print("%s" % data)
-        except IOError:
-                pass
-        print("Connection lost!")
-reciever()
-
-#cleanup
-client_sock.close()
-server_sock.close()
-print("all done")
+while (isinterupted == False):
+        SearchForDev()
