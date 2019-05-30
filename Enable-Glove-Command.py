e82033b6f1e4ad74a58d06fff2b347fc74b65f2c
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-05-30 10:11:54 -0500

    authenticate and open rfcomm port

--- Enable-Glove-Command.py
+++ Enable-Glove-Command.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-
-# Data Glove Commander BT Handler
-# James Andrews <jandrews7348@floridapoly.edu>
+'''
+        Data Glove Commander BT Handler
+        James Andrews <jandrews7348@floridapoly.edu>
+'''
 import time
 import sys
-#from bluetooth import *
 import bluetooth
 from uuid import uuid4
 from bluetooth.ble import DiscoveryService
 from bluetooth import *
 
-addr = None
 BaudRate = 19200
 MaxBytes = 100000
 interval = 100
-#uuid = "00001101-0000-10000-8000-008055f9b34fb"
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "00001101-0000-1000-8000-00805f9b34fb"
 addr = "00:80:E1:BC:4A:22"
+server_sock=BluetoothSocket( RFCOMM )
+
 currentPose = "__"
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 input = raw_input
@@ -32,20 +32,45 @@ def SearchForDev():
                 print("%s:%s" % (addr, name))
                 device = name
         if (len(nearby_devices)==0):
-                print("Glove out of range!")
+                print("[Err] Glove out of range!")
         elif (len(nearby_devices)==1):
-                print("found %d device" % len(nearby_devices))
+                print("[OK] Found %d device" % len(nearby_devices))
         else:
-                print("found %d devices" % len(nearby_devices))
+                print("Found %d devices" % len(nearby_devices))
 SearchForDev()
 
 def connect():
         # Establish RFCOMM Link
         service_matches = find_service( uuid = uuid, address = addr )
         if (len(service_matches)) == 0:
-                print("Unable to connect to glove! Exiting...")
+                print("[Err] Unable to connect to glove! Exiting...")
                 exit()
         elif (len(service_matches) == 1):
-                print("Locked on to commander glove!")
-
+                first_match = service_matches[0]
+                port = first_match["port"]
+                name = first_match["name"]
+                host = first_match["host"]
+                print("[OK] Locked on to %s!" % (name))
 connect()
+
+client_sock, client_info = server_sock.accept()
+print("[OK] Authenticated with", client_info)
+
+def reciever():
+        try:
+                while True:
+                        data = client_sock.recv(1024)
+                        if len(data) == 0: break
+                        if len(data) < MaxBytes: continue
+                        print("received [%s]" % data)
+        except IOError:
+                print("[Err] An error has occoured in connecting to the device.")
+                pass
+reciever()
+
+#Cleanup connections
+client_sock.close()
+server_sock.close()
+print("disconnected")
+
+exit()
