c19d4c42d1c0609f9e9733c282bc20ffa650cd9a
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-06-05 17:55:54 +0000

    configure gattlib for python3

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # PyBluez setup script
 # James Andrews <Jandrews7348@floridapoly.edu>
+sudo apt-get install python3
 sudo apt-get install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev
+#Configure gattlib for python3
+pip3 download gattlib
+tar xvzf ./gattlib-0.20150805.tar.gz
+rm gattlib-0.20150805.tar.gz
+chmod -Rv a+rwx gattlib-0.20150805/
+cd gattlib-0.20150805/
+sed -ie 's/boost_python-py34/boost_python-py37/' setup.py
+pip3 install .
 echo "done!"
