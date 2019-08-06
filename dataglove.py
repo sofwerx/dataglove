17ed5a204fb265dd366addba21a159d5478fb495
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-08-06 09:37:55 -0400

    2iC Fun call

---
+++ dataglove.py
@@ -0,0 +1,137 @@
+import serial
+import time
+import sys
+import threading
+#import numpy as np
+from subprocess import call
+
+#Rename to correct serial port
+device = '/dev/rfcomm0'
+#Make False for USB serial port
+bluetoothmode = True
+
+# © 2019 BeBop Sensors, Inc.
+data = []
+
+class GloveSerialListener(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+
+        self.glove = serial.Serial()
+        self.glove.baudrate = 460800
+        self.glove.port = device
+        self.glove.timeout = 1
+        self.glove.open()
+
+        self.data = []
+        self.data_shared = []
+
+    def parse(self, byte_to_parse):
+        global data
+
+        b = int.from_bytes(byte_to_parse, byteorder='big')
+        #print(b)
+        if b == 240:
+            self.data = []
+        elif b == 247:
+            self.data.append(b)
+
+            #might need some thread saftey here
+            if (self.data[0] == 1):
+                data = self.data
+
+        else:
+            self.data.append(b)
+
+    def run(self):
+
+        global data
+
+        if self.glove.is_open:
+            # data on
+            self.glove.write(bytearray([176, 115, 1]))
+
+            if (bluetoothmode != True):
+                self.glove.write(bytearray([176, 118, 1]))
+            elif (bluetoothmode == True):
+                self.glove.write(bytearray([176, 118, 2]))
+            else:
+                print("ERROR! bluetooth mode not set!")
+
+            while True:
+                self.parse(self.glove.read())
+
+        else:
+            self.close()
+
+def main():
+    data_glove_thread = GloveSerialListener(device)
+    data_glove_thread.start()
+
+    #Wait for data
+    if not data:
+        while True:
+            print("Waiting for data...")
+            time.sleep(2)
+            if data:
+                break
+
+
+    while True:
+
+        time.sleep(1)
+        if (data[0] == 1 and data[1] == 11):
+            #Finger Data
+            time.sleep(1)
+            thumb = (data[2] + data[3])
+            index = (data[4] + data[5])
+            middle = (data[6] + data[7])
+            ring = (data[8] + data[9])
+            pinky = (data[10] + data[11])
+            fingers = [thumb,index,middle,ring,pinky]
+            hand = sum([data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]])
+            #print(dtype,fingers,hand)
+            print(fingers)
+            #Defines current pose
+            pose = "ofnen"
+
+            if (hand >= 600 and thumb >= 30):
+                print("Fist")
+                #Call 2ic adaptor
+                call(["./gloveadaptor", "Fist"])
+                pose = "fist"
+            if (hand <= 500 and thumb >= 180):
+                print("Four")
+                call(["./gloveadaptor", "Four"])
+                pose = "four"
+            if (thumb <= 20 and index >= 140 and middle >= 140 and ring >= 100 and pinky >= 120):
+                print("Thumbs Up")
+                call(["./gloveadaptor", "ThumbsUp"])
+                pose = "thumbsup"
+            if (thumb >= 20 and index <= 20 and middle <= 20 and ring >= 120 and pinky >= 120):
+                print("Peace")
+                call(["./gloveadaptor", "Peace"])
+                pose = "peace"
+            if (index <= 20 and middle >= 140 and ring >= 140 and pinky <= 20):
+                print("Hook'em")
+                call(["./gloveadaptor", "Hookem"])
+                pose = "hookem"
+            # Add more poses here using same format
+
+            else:
+                pose = "ofnen"
+
+        elif (data[0] == 2 and data[1] == 12):
+            #Accelerometer Data
+            print(data[0])
+    data_glove_thread.close()
+
+#MainLoop
+while True:
+    try:
+        main()
+    except(OSError):
+        print("Failed to connect to glove. Retrying...")
+        time.sleep(1)
+    except(KeyboardInterrupt):
+        exit()
